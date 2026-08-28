086ca593f12d0f3c0c2ba109b0353849b12d6775
ruslandoga <ruslandoga+gh@icloud.com>
2026-08-28 23:16:36 +0300

    Add OpenTelemetry Collector test fixture

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,27 @@
+{:ok, _finch} = Finch.start_link(name: :default)
+
+request =
+  Finch.build(
+    :post,
+    "http://localhost:4318/v1/metrics",
+    [{"content-type", "application/x-protobuf"}],
+    <<>>
+  )
+
+case Finch.request(request, :default, receive_timeout: to_timeout(second: 1)) do
+  {:ok, %Finch.Response{status: 200}} ->
+    :ok
+
+  response ->
+    Mix.shell().error("""
+    OpenTelemetry Collector is not detected at localhost:4318! Please start the local container with the following command:
+
+        docker compose up -d collector
+
+    Collector response: #{inspect(response)}
+    """)
+
+    System.halt(1)
+end
+
 ExUnit.start()
